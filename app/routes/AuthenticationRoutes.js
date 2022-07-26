17ff4c490083d3920c5f64e176476a5d008d547c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 13:07:32 +0430

    Update routes

--- app/routes/AuthenticationRoutes.js
+++ app/routes/AuthenticationRoutes.js
@@ -4,5 +4,7 @@ const controllers = require("../controllers/AuthenticationControllers");
 const router = express.Router();
 
 router.post('/telegram', controllers.TelegramAuthentication);
+router.post('/register', controllers.EmailPasswordRegister);
+router.post('/login', controllers.EmailPasswordLogin);
 
 module.exports = router;
