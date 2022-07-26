80d0e54019284e8d6263940dc0f77d51a6d5b26c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 12:47:42 +0430

    Add sample models

---
+++ app/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userModule = new Schema({
+    tid: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    password: {
+        type: String,
+        required: false
+    },
+    name: {
+        type: String,
+        required: false
+    },
+}, {timestamps: true});
+
+const User = mongoose.model('user', userModule);
+
+module.exports = User;
