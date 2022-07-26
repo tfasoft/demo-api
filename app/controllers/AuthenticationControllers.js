14dae8f06fa436e5f3f9d5e7f75c4ff7c82e32e2
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 13:07:52 +0430

    Update controllers

--- app/controllers/AuthenticationControllers.js
+++ app/controllers/AuthenticationControllers.js
@@ -4,6 +4,20 @@ const TelegramAuthentication = (req, res) => {
     res.send('Telegram');
 }
 
+const EmailPasswordRegister = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.send('Email Password Register');
+}
+
+const EmailPasswordLogin = () => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.send('Email Password Login');
+}
+
 module.exports = {
     TelegramAuthentication,
+    EmailPasswordRegister,
+    EmailPasswordLogin,
 }
