d5b8c2c97b30097b87fa47587b1b581463bff798
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 14:17:34 +0430

    Update some stuff

--- app/controllers/AuthenticationControllers.js
+++ app/controllers/AuthenticationControllers.js
@@ -1,19 +1,28 @@
+const tfa = require('tfa-node-sdk');
+
+const User = require('../models/user');
+
 const TelegramAuthentication = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
-    res.send('Telegram');
+    const auth = new tfa("WuBjwvrQencoplabrUtPvDKaz");
+    const result = auth.authUser(req.body.tid);
+
+    result.then((response) => {
+       res.send(response);
+    });
 }
 
 const EmailPasswordRegister = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
-    res.send('Email Password Register');
+    res.send(req.body);
 }
 
-const EmailPasswordLogin = () => {
+const EmailPasswordLogin = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
-    res.send('Email Password Login');
+    res.send(req.body);
 }
 
 module.exports = {
