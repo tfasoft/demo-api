79b9ff8fe138e0e93168472dfe2ec4ab4abab643
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 15:45:15 +0430

    Completed the Telegram Authentication

--- app/controllers/AuthenticationControllers.js
+++ app/controllers/AuthenticationControllers.js
@@ -6,10 +6,47 @@ const TelegramAuthentication = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
     const auth = new tfa("WuBjwvrQencoplabrUtPvDKaz");
-    const result = auth.authUser(req.body.tid);
+    const result = auth.authUser(req.body.user_token);
 
     result.then((response) => {
-       res.send(response);
+       const statCode = response.status;
+       const data = response.data;
+
+       if (statCode === 200) {
+           User.findOne({ tid: data.user.uid })
+               .then((result) => {
+                   const userData = {
+                       tid: data.user.uid,
+                   };
+
+                   if (result === null) {
+                       const user = new User(userData);
+                       user.save()
+                           .then((userFromMongo) => {
+                               res.status(200);
+                               res.send(userFromMongo);
+                           })
+                           .catch((error) => {
+                               res.send(error);
+                           });
+                   } else {
+                       User.findOne(req.body)
+                           .then((userFromMongo) => {
+                               res.status(200);
+                               res.send(userFromMongo);
+                           })
+                           .catch((error) => {
+                               res.send(error)
+                           });
+                   }
+               })
+               .catch((error) => {
+                   res.send(error);
+               })
+       } else {
+           res.status(401);
+           res.send(data);
+       }
     });
 }
 
@@ -23,7 +60,7 @@ const EmailPasswordRegister = (req, res) => {
         })
         .catch((error) => {
             res.send(error);
-        })
+        });
 }
 
 const EmailPasswordLogin = (req, res) => {
