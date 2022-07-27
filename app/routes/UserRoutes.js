0da07b8c8050ec19a22f902c96aa75e37b557054
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-27 10:05:32 +0430

    Create update data router

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -4,5 +4,6 @@ const controllers = require('../controllers/UserControllers');
 const router = express.Router();
 
 router.post('/info', controllers.info);
+router.post('/update', controllers.update);
 
 module.exports = router;
