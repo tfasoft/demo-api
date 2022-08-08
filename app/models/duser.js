8e75ac1d27b1eff67e7d915f6ec17faeb1513ded
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 23:57:34 +0430

    Change model

--- app/models/duser.js
+++ app/models/duser.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userModule = new Schema({
+const UserModule = new Schema({
     tid: {
         type: String,
         required: false,
@@ -25,6 +25,6 @@ const userModule = new Schema({
     },
 }, {timestamps: true});
 
-const User = mongoose.model('user', userModule);
+const User = mongoose.model('user', UserModule);
 
 module.exports = User;
